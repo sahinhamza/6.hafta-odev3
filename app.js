aed8806f243efeff279572fba258d4a9716354fb
Hamza Sahin <hmzshn_11@hotmail.com>
2022-04-19 23:08:58 +0300

    javascript file changed

--- app.js
+++ app.js
@@ -81,3 +81,60 @@ const menu = [
     desc: `Red bean paste dessert, serving with honey.`,
   },
 ];
+
+
+// nav bar
+const mutfaklar = ["All", "Korea", "Japan", "China"]
+
+const mutfaklarList = () => {
+  const mutfaklarBtn = mutfaklar
+    .map((category) => {
+      return `<button class="btn btn-outline-dark btn-item" data-id=${category}>${category}</button>`;
+    })
+    .join("");  
+
+  const btnContainer = document.querySelector(".btn-container");
+  btnContainer.innerHTML = mutfaklarBtn;
+
+  //mutfak filtreleme
+  const filtrele = document.querySelectorAll(".btn-item");
+ filtrele.forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+      const category = e.currentTarget.dataset.id;
+      const mutfakGoster = menu.filter((item) => {
+        if (item.category === category) {
+          return item;
+        }
+      });
+      if (category === "All") {
+        listele(menu);
+      } else {
+        listele(mutfakGoster);
+      }
+    });
+  });
+};
+
+// yemekler
+const listele = (item) => {
+  let menuGoster = item.map((item) => {
+    return `<div class="menu-items col-lg-6 col-sm-12">
+            <img src=${item.img} alt=${item.title} class="photo" />
+            <div class="menu-info">
+              <div class="menu-title">
+                <h4>${item.title}</h4>
+                <h4 class="price">${item.price}</h4>
+              </div>
+              <div class="menu-text">
+                ${item.desc}
+              </div>
+            </div>
+          </div>`
+  });
+  menuGoster = menuGoster.join("");
+  const section = document.querySelector(".section-center");
+  section.innerHTML = menuGoster;
+};
+
+listele(menu);
+mutfaklarList();
